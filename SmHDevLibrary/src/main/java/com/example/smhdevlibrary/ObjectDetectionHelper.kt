e08b247cd44bc2655c345bc199cdd8d52860e101
pedrolobo98 <pedrowolf98@hotmail.com>
2022-01-12 15:38:45 +0000

    area detected15

--- SmHDevLibrary/src/main/java/com/example/smhdevlibrary/ObjectDetectionHelper.kt
+++ SmHDevLibrary/src/main/java/com/example/smhdevlibrary/ObjectDetectionHelper.kt
@@ -63,7 +63,7 @@ class ObjectDetectionHelper(context: Context) {
         var BuildList = buildList(bitmap, resultToDisplay)
         var FilteresArea = conditionTree.filterByArea(BuildList)
         var (resizeBoundinBox, img, area) = conditionTree.resizeByBoundingBoxLimits(FilteresArea)
-        if(area > (bitmap.height * bitmap.width)/10){
+        if(area > (bitmap.height * bitmap.width)/15){
             if (resizeBoundinBox.size != 0){
                 when(mode) {
                     1 -> return Utils.output(
